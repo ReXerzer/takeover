54c78723a70e7f6f13524cafae47063d68b5e95b
reXez <28502942+ReXerzer@users.noreply.github.com>
2017-07-23 22:18:18 +0200

    Update kick.py

--- lib/kick.py
+++ lib/kick.py
@@ -5,7 +5,7 @@ import sys
 import logging
 
 conf.verb = 0
-logging.basicConfig(level=logging.INFO, format="[{0}%(levelname)s{1}][{2}%(asctime)s{1}]{3}%(message)s{1}".format(LGREEN, RST, BLUE, RD), datefmt="%H:%M:%S")
+logging.basicConfig(level=logging.DEBUG, format="[{0}%(levelname)s{1}][{2}%(asctime)s{1}]{3}%(message)s{1}".format(LGREEN, RST, BLUE, RD), datefmt="%H:%M:%S")
 
 def gatewayIP():
     pkt = IP(dst="www.google.com", ttl=0)/TCP()
@@ -36,13 +36,12 @@ class kickout:
             logging.info("{0}[{1}+{0}] {2}MAC address found:{0}".format(RST, LGREEN, RD))
             logging.info("{0}[{3}*{0}] {4}Gateway:{0} {1}{2}{0}".format(RST, GR, self.gateMAC, BLUE, RD))
             logging.info("{0}[{3}*{0}] {4}Target:{0} {1}{2}{0}".format(RST, GR, self.tgtMAC, BLUE, RD))
-        logging.info("{0}[{1}+{0}] {2}Kicking target...{0}".format(RST, LGREEN, RD))
+        logging.info("{0}[{1}+{0}] {2}Kicking target: {3}...{0}".format(RST, LGREEN, RD, self.tgtIP))
         logging.info("{0}[{1}+{0}] {2}Successfully kicked target{0}".format(RST, LGREEN, RD))
         count = 1
         while True:
             try:
                 self.kickone()
-                #logging.info("\rTarget kicked for {} seconds".format(count))
                 sys.stdout.write("\r[{4}INFO{0}][{3}{2}{0}]{1} Seconds kicked{0} (Press CTRL+C to cancel)".format(RST, RD, count, GR, LGREEN))
                 sys.stdout.flush()
                 count += 2
